c9862e2cb6d871ab479498f2b962dc2211b3e261
Bilawal Yousaf <bilawalkhan57777@gmail.com>
2026-06-16 19:18:48 +0500

    getting weather from location funtionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import "./style.scss";
-import react, { useState, useRef } from "react";
+import react, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import WeatherCard from "./component/weather";
 
 const App = () => {
   const inputRef = useRef();
-
   const [weather, set_weather] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect (() => {
+      getWeatherFromLocation()
+  },[])
+
+  const getWeatherFromLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async (location) => {
+          const latitude = location.coords.latitude;
+          const longitude = location.coords.longitude;
+
+          try {
+            setLoading(true);
+            const apiKey = "a3f338eb00c448a728f0699e0d07c6a1";
+             const response = await axios.get(
+              `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`,
+           );
+                 set_weather([response.data, ...weather]);
+                 setLoading(false);
+
+          } catch (error) {
+            console.error(error);
+            setLoading(false);
+            
+          }
+
+        });
+      }
+  }
+
 
   const get_weather = async (e) => {
     e.preventDefault();
@@ -27,17 +57,26 @@ const App = () => {
   return (
     <>
       <h1>Fast Wheater App</h1>
+      
 
       <form onSubmit={get_weather}>
         <input type="text" placeholder="Enter Your City Name" ref={inputRef} required />
         <button>Get Wheather</button>
       </form>
+      
+      {loading && (
+        <div className="loader-container">
+        <div className="loader"></div>
+    </div>
+  )}
 
-      <div className="weather-container">
+      {!loading && (
+        <div className="weather-container">
         {weather.map((singleData, index) => {
           return <WeatherCard key={index} data={singleData} />;
-        })}
-      </div>
+      })}
+    </div>
+  )}
     </>
   );
 };
